9dd98a1f0cab5a854e9136613dbaee7c3aca8e84
Sarah <salhi0844@gmail.com>
2024-03-20 17:04:47 +0100

    1ere version logique du laser

--- src/main/java/piece/Laser.java
+++ src/main/java/piece/Laser.java
@@ -1,46 +1,7 @@
 package src.main.java.piece;
 
-import src.main.java.enums.*;
+import src.main.java.enums.Direction;
 
 public class Laser {
-    private int positionI;
-    private int positionJ;
     private Direction direction;
-
-    public Laser(int positionI, int positionJ, Direction direction) {
-        this.positionI = positionI;
-        this.positionJ = positionJ;
-        this.direction = direction;
-    }
-
-    public void changerDirection(Direction nouvelleDirection) {
-        this.direction = nouvelleDirection;
-    }
-
-    // Getters et setters
-    public int getPositionI() {
-        return positionI;
-    }
-
-    public void setPositionI(int positionI) {
-        this.positionI = positionI;
-    }
-
-    public int getPositionJ() {
-        return positionJ;
-    }
-
-    public void setPositionJ(int positionJ) {
-        this.positionJ = positionJ;
-    }
-
-    public Direction getDirection() {
-        return direction;
-    }
-
-    // Méthodes pour déplacer le laser selon sa direction actuelle
-    public void deplacer() {
-        this.positionI += this.direction.getDi();
-        this.positionJ += this.direction.getDj();
-    }    
 }
---
+++ src/main/java/piece/TreeNode.java
@@ -0,0 +1,11 @@
+public class TreeNode {
+    String direction;
+    TreeNode left;
+    TreeNode right;
+
+    public TreeNode(String direction) {
+        this.direction = direction;
+        this.left = null;
+        this.right = null;
+    }
+}
